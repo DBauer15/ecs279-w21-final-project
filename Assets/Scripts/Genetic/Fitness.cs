bd1e7a21ea8197501c6e07ddb7c176c06f0db9bc
David Bauer <davbauer@ucdavis.edu>
2021-03-10 17:55:34 +0100

    Added basic fitness function

--- Assets/Scripts/Genetic/Fitness.cs
+++ Assets/Scripts/Genetic/Fitness.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class Fitness : MonoBehaviour
 {
+    FitnessSensor[] sensors;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sensors = GetComponentsInChildren<FitnessSensor>();
+        InvokeRepeating("PrintFitness", 0.1f, 1);
+    }
+
+    public float GetFitness()
+    {
+        float fitness = 0;
+        foreach(FitnessSensor sensor in sensors) {
+            fitness += sensor.GetFitness();
+        }
+
+        return fitness;
+    }
+
+    public void PrintFitness()
+    {
+        print(GetFitness());
     }
 
     // Update is called once per frame

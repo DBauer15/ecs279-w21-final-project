a4ad93c109689dd9771fec6a5161c69511dffa1c
David Bauer <davbauer@ucdavis.edu>
2021-03-11 20:35:35 +0100

    Basic Serializer

---
+++ Assets/Scripts/DNASerializer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DNASerializer
+{
+    public static string Serialize<T> (DNA<T> dna) where T : Gene, new()
+    {
+        return JsonUtility.ToJson(dna, true);
+    }
+
+    public static DNA<T> Deserialize<T> (string dnaJson) where T : Gene, new()
+    {
+        return JsonUtility.FromJson<DNA<T>>(dnaJson);
+    }
+}
